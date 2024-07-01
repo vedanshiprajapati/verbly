b1258442943830c7d9098af4f1ec985954e58e9f
Kartik Suthar <kartikgajjar2022@gmail.com>
2024-07-01 17:36:54 +0530

    solved error: useNavigate() may be used only in the context of a <Router> component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Blog from "./components/pages/Blog";
-import Home from "./components/pages/Home";
-import { ThemeProvider } from "./components/context/ThemeProvider";
-import Login from "./components/pages/Auth";
-import NavBar from "./components/pages/NavBar";
-import BlogEdit from "./components/pages/BlogEdit";
-import { AuthProvider } from "./components/context/AuthContext";
-import ProtectedRoute from "./components/pages/ProtectedRoute";
+import { Route, Routes } from "react-router-dom"
+import Blog from "./components/pages/Blog"
+import Home from "./components/pages/Home"
+import { ThemeProvider } from "./components/context/ThemeProvider"
+import Login from "./components/pages/Auth"
+import NavBar from "./components/pages/NavBar"
+import BlogEdit from "./components/pages/BlogEdit"
+import { AuthProvider } from "./components/context/AuthContext"
+import ProtectedRoute from "./components/pages/ProtectedRoute"
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <AuthProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<ProtectedRoute />}>
-                <Route path="/" element={<NavBar />}>
-                  <Route path="/blog/edit" element={<BlogEdit />} />
-                  <Route path="/blog/:id" element={<Blog />} />
-                  <Route path="/" element={<Home />} />
-                </Route>
+          <Routes>
+            <Route path="/" element={<ProtectedRoute />}>
+              <Route path="/" element={<NavBar />}>
+                <Route path="/blog/edit" element={<BlogEdit />} />
+                <Route path="/blog/:id" element={<Blog />} />
+                <Route path="/" element={<Home />} />
               </Route>
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </BrowserRouter>
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </AuthProvider>
       </ThemeProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
